da181c989a6dbadc9c5d2327f2092e7348927c44
Pall Pandiyan <pallpandiyan@gmail.com>
2022-03-15 12:29:35 +0530

    First commit

---
+++ booleanOR.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+// this c program demonstrate how boolean or operation runs only once if the first condition is true
+// the remaining comparision simply not executed
+
+int f1 (int a, int b)
+{
+    if (a > b)
+    {
+        printf("A is greater than B\n");
+        return 1;
+    }
+    else
+    {
+        printf("B is greater than A");
+        return 0;
+    }
+}
+
+void main()
+{
+    if (f1(20,10) || f1(10,20))
+        printf("C is fun!\n");
+}
+
+// the output will be..
+// A is greater than B
+// C is fun!
