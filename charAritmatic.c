5d963954efaf9f37d1e09fe95a4c7bfff464f135
Pall Pandiyan <pallpandiyan@gmail.com>
2022-03-16 20:46:21 +0530

    deleted a duplicate topic, and merge their contents.

--- charAritmatic.c
+++ charAritmatic.c
@@ -1,6 +1,16 @@
+// char data type can store a character i.e: 'a' or '8'
+// even '\n' (newline character) '\t' (tab character)
+// we can also access the int value of the character while
+// printing using '%d'. we can also perform +/- (addition
+// or subtraction) in char data type.
+
 #include<stdio.h>
-void main(){
-    char c1 ='a';
-    char c2 = c1+10;
-    printf("%c",c2);
+void main() {
+    char c1='a', c2='A';
+    int i=c2-c1;
+    printf("A-a as int : %d\n", i);
+    //printf("%d",(int)'A');
+    c2 = c1+10;
+    printf("a+10 as char: %c\n",c2);
+    printf("a+10 as int : %d\n",c2);
 }
--- characterAritmatic.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-void main() {
-    char c1='a' , c2='A';
-    int i=c2-c1;
-    printf("%d\n", i);
-    //printf("%d",(int)'A');
-}
