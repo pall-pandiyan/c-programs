ae7c05f3233d6b607481feecb0873629190cb70b
Pall Pandiyan <pallpandiyan@gmail.com>
2022-03-24 16:34:05 +0530

    provided explanation and sample output

--- shiftLeft.c
+++ shiftLeft.c
@@ -1,8 +1,20 @@
+// the "<<" is called shift left operator, there is also a ">>" shift right operator
+// basically all this operator does is shifting binary value to the given target
+// in this example -1 (0001) will be shifted left 3 times into -8 (1000)
+// %x is format specifier for unsigned hex, so minus values connts down from hex value
+// 'ffffffff' is -1 and 'fffffff8' is -8
+
 #include<stdio.h>
 
 int main() {
+    printf("%x\n",-1);
     printf("%x \n",-1<<3);
-    printf("%x \n",-1<<3);
-    printf("%x \n",-1<<3);
+    printf("%d \n",-1<<3);
+    printf("\n");
     return 1;
 }
+
+// the output will be..
+// ffffffff
+// fffffff8 
+// -8
--- strManipulation.c
+++ strManipulation.c
@@ -1,6 +1,22 @@
-// 
+// we can directly print a char[] without specifing any format specifier if we only want to print that char[]
+// we cannot even include '\n' (newline charactor)
+// we can also use "%s" format specifier for char[]
+// "%c" for a char
+
 #include<stdio.h>
 void main() {
     char *string[20] = { "one", "two", "three"};
+    char helllo[10] = "Helo!";
+    printf(helllo);
+    printf("\n");
     printf(string[0]);
+    printf("\n");
+    printf("%s\n",string[1]);
+    printf("%c\n",string[2][2]);
 }
+
+// the output will be..
+// Helo!
+// one
+// two
+// r
